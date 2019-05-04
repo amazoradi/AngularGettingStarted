ba72b74a7af4956e6c98cd754da187f50915e16c
Austin Zoradi <amazoradi@gmail.com>
2019-05-04 12:36:39 -0500

    added product list component, through mod 5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,9 @@ import { Component } from "@angular/core";
   //derective name
   selector: 'pm-root',
   //template being used by this component
+  // template: `<div><h1>{{pageTitle}}</h1>
+  //             <div> My first Component</div>
+  //             </div>`
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './products/product-list.component';
 
 
 //id class as a module using the ngmodule decorator and pass in meta data
 @NgModule({
   //which components belong to this module
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent 
   ],
   //external modules we want avialiable to all componentns of this module
   imports: [
